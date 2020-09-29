5ef38b1e8cc2031ca86e8b306c3e25b27733ed02
Felix (xq) Queißner <git@mq32.de>
2020-09-29 17:20:40 +0200

    Fixes string problem.

--- workbench/src/windows/graphic/imagesource.cpp
+++ workbench/src/windows/graphic/imagesource.cpp
@@ -6,44 +6,43 @@ REGISTER_WINDOW_CLASS(ImageSource, Menu::Graphic, "image", "Image")
 
 #include "fileio.hpp"
 
-ImageSource::ImageSource() :
-    Window("Image", ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar),
-    img(0),
-    currentFile("")
+ImageSource::ImageSource() : Window("Image", ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_MenuBar),
+														 img(0),
+														 currentFile("")
 {
 	this->AddSource(new Source(CgDataType::Texture2D, "Texture", &this->img));
 }
 
 ImageSource::~ImageSource()
 {
-	if(this->img != 0)
+	if (this->img != 0)
 		glDeleteTextures(1, &this->img);
 }
 
 void ImageSource::OnUpdate()
 {
-	if(ImGui::BeginMenuBar())
+	if (ImGui::BeginMenuBar())
 	{
-		if(ImGui::BeginMenu("Image"))
+		if (ImGui::BeginMenu("Image"))
 		{
-			if(ImGui::MenuItem("Load..."))
+			if (ImGui::MenuItem("Load..."))
 				this->LoadFile();
 			ImGui::EndMenu();
 		}
 		ImGui::EndMenuBar();
 	}
 
-	if(this->img != 0)
+	if (this->img != 0)
 	{
 		int w, h;
 		glGetTextureLevelParameteriv(this->img, 0, GL_TEXTURE_WIDTH, &w);
 		glGetTextureLevelParameteriv(this->img, 0, GL_TEXTURE_HEIGHT, &h);
 
-        ImGui::Image(
-			ImTextureID(uintptr_t(this->img)),
-            ImVec2(256, 256 * h / w),
-            ImVec2(0,1),
-            ImVec2(1,0));
+		ImGui::Image(
+				ImTextureID(uintptr_t(this->img)),
+				ImVec2(256, 256 * h / w),
+				ImVec2(0, 1),
+				ImVec2(1, 0));
 
 		this->imgSettings.Show();
 	}
@@ -55,51 +54,49 @@ void ImageSource::OnUpdate()
 
 void ImageSource::SetTexture(GLuint tex)
 {
-	if(this->img == tex)
+	if (this->img == tex)
 		return;
-	if(this->img != 0)
+	if (this->img != 0)
 		glDeleteTextures(1, &this->img);
 
 	this->img = tex;
 	imgSettings.SetTexture(tex);
 }
 
-
-
 nlohmann::json ImageSource::Serialize() const
 {
 	return {
-		{ "filter", this->imgSettings.Serialize() },
-		{ "file", this->currentFile },
+			{"filter", this->imgSettings.Serialize()},
+			{"file", this->currentFile},
 	};
 }
 
 void ImageSource::Deserialize(const nlohmann::json &value)
 {
-	this->currentFile = value["file"];
-	if(this->currentFile.length() == 0)
+	this->currentFile = value["file"].get<std::string>();
+	if (this->currentFile.length() == 0)
 		return;
 
 	GLuint i = load_texture(this->currentFile);
-	if(i == 0)
+	if (i == 0)
 		return;
 	this->SetTexture(i);
 
-
-	try {
+	try
+	{
 		this->imgSettings.Deserialize(value.at("filter"));
 	}
-	catch(nlohmann::detail::out_of_range const & ex) {
-
+	catch (nlohmann::detail::out_of_range const &ex)
+	{
 	}
 }
 
-
 void ImageSource::LoadFile()
 {
-	auto path =FileIO::OpenDialog("png,jpeg,jpg,bmp");
+	auto path = FileIO::OpenDialog("png,jpeg,jpg,bmp");
 	GLuint i = load_texture(path);
-	if(i != 0) {
+	if (i != 0)
+	{
 		this->SetTexture(i);
 		this->currentFile = path;
 	}
