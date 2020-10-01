84d6626cae749bf32650fe63d2a2ac36d32d1791
Felix (xq) Queißner <git@mq32.de>
2020-10-01 20:12:25 +0200

    Adds release builder script.

---
+++ prepare-release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eo pipefail
+
+ARTIFACT=workbench-release.zip
+
+# Test if we have all required programs
+which 7z zig > /dev/null
+
+echo "Building linux executable"
+zig build -Dtarget=x86_64-native-gnu -Drelease-safe -Dstrip
+
+echo "Building windows executable"
+zig build -Dtarget=x86_64-windows-gnu -Drelease-safe -Dstrip
+
+rm -f "${ARTIFACT}"
+rm -rf release
+mkdir -p release
+
+cp -r examples release/
+cp -r templates release/
+cp LICENSE MANUAL.txt release/
+cp zig-cache/bin/cg-workbench release/
+cp zig-cache/bin/cg-workbench.exe release/
+
+cd release
+7z a "../${ARTIFACT}" *
+cd ..
